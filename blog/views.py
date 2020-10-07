7d260694d8f4a3913ccfabc1e101fecc522d75fb
igorworkpc <sonikry.99@gmail.com>
2020-10-07 17:08:52 +0500

    added the ability to comment on posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -36,8 +38,13 @@ def post_share(request, post_id):
         return render(request, "blog/post/share.html", {"post": post, "form": form, "sent": sent})
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
     paginator = Paginator(object_list, 3)   # По 3 статьи на каждой странице
     page = request.GET.get("page")
     try:
@@ -48,13 +55,35 @@ def post_list(request):
     except EmptyPage:
         # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"page": page, "posts": posts})
+    return render(request, "blog/post/list.html", {"page": page, "posts": posts, "tag": tag})
 
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status="published", publish__year=year,
                              publish__month=month, publish__day=day)
-    return render(request, "blog/post/detail.html", {"post": post})
+    # Список активных комментариев для этой статьи
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == "POST":
+        # Пользователь отправил комментарий
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Создаем комментарий, но пока не сохраняем в базе данных
+            new_comment = comment_form.save(commit=False)
+            # Привязываем комментарий к текущей статье
+            new_comment.post = post
+            # Сохраняем комментарий в базе данных
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by("-same_tags", "-publish")[:4]
+    return render(request, "blog/post/detail.html", {"post": post,
+                                                     "comments": comments,
+                                                     "new_comment": new_comment,
+                                                     "comment_form": comment_form,
+                                                     "similar_posts": similar_posts})
 
 
 
